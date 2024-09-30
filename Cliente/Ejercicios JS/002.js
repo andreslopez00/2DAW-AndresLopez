92ff4610b85aa66a63d7ad69665dba52d0aa6eab
Andrés López Barrera <andreslb2000@gmail.com>
2024-09-30 13:46:21 +0200

    Delete Cliente/Ejercicios JS/002.js

--- Cliente/Ejercicios JS/002.js
+++
@@ -1,9 +0,0 @@
-function compararNombres() {
-    var nombre1 = "Manolo";
-    var nombre2 = "OC";
-    if (nombre1 === nombre2) {
-        alert("Los nombres coinciden");
-    } else {
-        alert("Los nombres no coinciden");
-    }
-}
